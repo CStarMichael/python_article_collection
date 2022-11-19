4a6ea53cf6086198fa0aa279b1edfe0f55ba5c79
Michael <alx1990cai@foxmail.com>
2022-11-19 18:21:06 +0800

    飞速搞定Excel+Python 练习

--- chapter2_plug/random_data_faker.py
+++ chapter2_plug/random_data_faker.py
@@ -6,7 +6,7 @@ Faker.seed(0)
 
 
 def get_data():
-    key_list = ["姓名", "详细地址", "所在省份", "手机号", "身份证号", "出生年月", "邮箱"]
+    key_list = ["姓名", "详细地址", "所在省份", "手机号", "身份证号", "出生年月", "邮箱","Time"]
     name = fake.name()
     address = fake.address()
     province = address[:3]
@@ -14,14 +14,16 @@ def get_data():
     id_card = fake.ssn()
     birth_date = id_card[6:14]
     email = fake.email()
-    info_list = [name, address, province, number, id_card, birth_date, email]
+    time = fake.date() + ' ' + fake.time()
+    info_list = [name, address, province, number, id_card, birth_date, email, time]
     person_info = dict(zip(key_list, info_list))
     return person_info
 
 
-df = pd.DataFrame(columns=["姓名", "详细地址", "所在省份", "手机号", "身份证号", "出生年月", "邮箱"])
-for i in range(10000):
+df = pd.DataFrame(columns=["姓名", "详细地址", "所在省份", "手机号", "身份证号", "出生年月", "邮箱","Time"])
+for i in range(1000):
     person_info = [get_data()]
     df1 = pd.DataFrame(person_info)
     df = pd.concat([df, df1])
-df.to_excel("模拟数据.xlsx", index=None)
+# df.to_excel("模拟数据.xlsx", index=None)
+df.to_csv("模拟数据.csv", index=None)
